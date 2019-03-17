a00dc7d3e3eeaeb1ab764282b421b8440ecb719d
unknown <jinghao1997@outlook.com>
2019-03-18 00:40:53 +1100

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:dio/dio.dart';
+import 'ui/LoginPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // 应用名称
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // 蓝色主题
+        primarySwatch: Colors.blue,
+      ),
+      // 注册路由表
+      routes: {
+        "new_page": (context) => LoginPage(),
+      },
+      // 应用路由主页
+      home: MyHomePage(title: 'Flutter Demo 主页'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // MyHomePage 是应用的首页. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  void getHttp() async {
+    String args =
+        "https://free-api.heweather.net/s6/weather/now?key=9b8e0d5e02244c0a9a6d22543afdc4a5&location=北京";
+    try {
+      Response response = await Dio().get(args);
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    SvgPicture cakeRectangle =
+    new SvgPicture.asset("assets/images/cake_rectangle.svg");
+
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+            SizedBox(
+            width: 50.0,
+            height: 50.0,
+            child: Image.asset("assets/images/cake_cup.png"),
+          ),
+          SizedBox(
+            width: 50.0,
+            height: 50.0,
+            child: cakeRectangle,
+          ),
+          RandomWordsWidget(),
+          Text.rich(TextSpan(children: [
+            TextSpan(text: "Home: "),
+            TextSpan(
+              text: "https://flutterchina.club",
+              style: TextStyle(color: Colors.blue),
+            ),
+          ])),
+          Row(
+            // Column is also layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '您已点击按钮',
+              ),
+              Text(
+                '$_counter',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .display1,
+              ),
+              Text('次'),
+            ],
+          ),
+          FlatButton(
+            child: Text("打开新页面"),
+            textColor: Colors.blue,
+            onPressed: () {
+              Navigator.pushNamed(context, "new_page");
+            },
+          ),
+          FlatButton(
+            color: Colors.blue,
+            child: Text("open echo route"),
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            onPressed: () {
+              Navigator.push(context, new MaterialPageRoute(builder: (context) {
+                return new EchoRoute("$_counter");
+              }));
+            },
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text("Submit"),
+            highlightColor: Colors.blue[700],
+            colorBrightness: Brightness.dark,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            onPressed: () {
+              getHttp();
+            },
+          ),
+          SwitchAndCheckBoxTestRoute(),
+          ],
+        )),
+    floatingActionButton: FloatingActionButton(
+    onPressed: _incrementCounter,
+    tooltip: 'Increment',
+    child: Icon(Icons.add
+    )
+    ,
+    )
+    , // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(
+        wordPair.toString() * 6,
+        style: TextStyle(fontSize: 16.0),
+      ),
+    );
+  }
+}
+
+class EchoRoute extends StatelessWidget {
+  final String tip;
+
+  EchoRoute(this.tip);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Echo route"),
+      ),
+      body: Center(
+        child: Text(tip),
+      ),
+    );
+  }
+}
+
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  @override
+  _SwitchAndCheckBoxTestRouteState createState() =>
+      new _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState
+    extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected = true; // 维护单选开关状态
+  bool _checkboxSelected = true; // 维护复选框状态
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected,
+          onChanged: (value) {
+            setState(() {
+              _switchSelected = value;
+            });
+          },
+        ),
+        Checkbox(
+          value: _checkboxSelected,
+          onChanged: (value) {
+            setState(() {
+              _checkboxSelected = value;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
---
+++ lib/ui/LoginPage.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  GlobalKey _formKey = new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("登录"),
+      ),
+      body: body(),
+    );
+  }
+
+  Widget body() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+      child: Form(
+        key: _formKey,
+        autovalidate: true,
+        child: Column(
+          children: <Widget>[
+            // 用户名
+            TextFormField(
+              autofocus: true,
+              controller: _usernameController,
+              decoration: InputDecoration(
+                  labelText: "用户名",
+                  hintText: "用户名或邮箱",
+                  icon: Icon(Icons.person)),
+              validator: (v) {
+                return v.trim().length > 0 ? null : "用户名不能为空";
+              },
+            ),
+
+            // 密码
+            TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    labelText: "密码",
+                    hintText: "您的登录密码",
+                    icon: Icon(Icons.lock)),
+                obscureText: true,
+                //校验密码
+                validator: (v) {
+                  return v.trim().length > 5 ? null : "密码不能少于6位";
+                }),
+
+            // 登录按钮
+            Padding(
+              padding: EdgeInsets.only(top: 28.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("登录"),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          //在这里不能通过此方式获取FormState，context不对
+                          //print(Form.of(context));
+
+                          // 通过_formKey.currentState 获取FormState后，
+                          // 调用validate()方法校验用户名密码是否合法，校验
+                          // 通过后再提交数据。
+                          if ((_formKey.currentState as FormState).validate()) {
+                            //验证通过提交数据
+                          }
+                        }),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
